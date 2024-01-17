f1269011a0fef78cd980fac84ba823c2c517b069
glackyy <othmanesticky@gmail.com>
2024-01-17 21:56:46 +0100

    0-uniq-users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -3,6 +3,6 @@
 
 CREATE TABLE IF NOT EXISTS users (
         id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        email varchar(250) NOT NULL UNIQUE,
-        name varchar(250),
+        email varchar(255) NOT NULL UNIQUE,
+        name varchar(255),
 )
