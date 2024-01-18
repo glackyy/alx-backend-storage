a8451bf7204acf7bf786690f7019cb85b60e19ad
glackyy <othmanesticky@gmail.com>
2024-01-18 10:24:23 +0100

    100-average_weight

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -4,9 +4,9 @@
 -- and weights of the user from the corrections table, summing up the total weight
 -- and then dividing the total weighted score by the total weight, and then updating
 -- the average score field of the user in the users table
-DROP PROCEDURE IF EXISTS ComputerAverageWeightedScoreForUser;
-DELIMITER $$ ;
-CREATE PROCEDURE ComputerAverageWeightedScoreForUser (user_id INTEGER)
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
     DECLARE total_weighted_score INT DEFAULT 0;
     DECLARE total_weight INT DEFAULT 0;
@@ -17,12 +17,14 @@ BEGIN
             INNER JOIN projects
                 ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
+
     SELECT SUM(projects.weight)
         INTO total_weight
         FROM corrections
             INNER JOIN projects
                 ON corrections.project_id = projects.id
         WHERE corrections.user_id = user_id;
+
     IF total_weight = 0 THEN
         UPDATE users
             SET users.average_score = 0
@@ -32,5 +34,5 @@ BEGIN
             SET users.average_score = total_weighted_score / total_weight
             WHERE users.id = user_id;
     END IF;
-END;$$
+END $$
 DELIMITER ;
