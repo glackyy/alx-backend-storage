c4055082ffdfcf1eca31e25d4c8df7cbdd341fba
glackyy <othmanesticky@gmail.com>
2024-01-18 08:59:53 +0100

    10-div

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,19 @@
+-- A Script that creates a function SafeDiv
+-- Divides and returns the first by the second number
+-- or returns 0 if the second number is equal to 0
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+    a INTEGER,
+    b INTEGER,
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    DECLARE res FLOAT;
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    SET res = (a * 1.0) / b;
+    RETURN res;
+END;$$
+DELIMITER ;
