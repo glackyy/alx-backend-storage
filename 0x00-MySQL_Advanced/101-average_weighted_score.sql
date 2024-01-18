b340aa2eb2394784d818e66dfbf49265a278f56c
glackyy <othmanesticky@gmail.com>
2024-01-18 10:37:06 +0100

    101-average_weight

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -4,12 +4,13 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 DELIMITER |
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    UPDATE users
-        (SELECT users.id, SUM(score * weight) / SUM(weight) AS w_average
-        FROM users JOIN corrections ON users.id = corrections.user_id
-        JOIN projects ON corrections.project_id = projects.id
-        GROUP BY users.id)
-    AS w_a
-    SET users.average_score = w_a.w_average
-    WHERE users.id = w_a.id;
+  UPDATE users AS us, 
+    (SELECT us.id, SUM(score * weight) / SUM(weight) AS w_avg 
+    FROM users AS us
+    JOIN corrections as c ON us.id=c.user_id 
+    JOIN projects AS pr ON c.project_id=pr.id 
+    GROUP BY us.id)
+  AS WA
+  SET us.average_score = WA.w_avg 
+  WHERE us.id=WA.id;
 END;
