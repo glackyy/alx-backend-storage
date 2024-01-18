204408911718b9eaf7e7550b9e420f52f8db7a1d
glackyy <othmanesticky@gmail.com>
2024-01-18 08:11:54 +0100

    2-fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,7 @@
 -- A Script that selects origin column, and sum of fans
 -- grouped by origin and ordered by num of fans desc
 
-SELECT origin, SUM(fans) AS numb_fans
+SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin
-    ORDER BY numb_fans DESC;
+    ORDER BY nb_fans DESC;
