1d7a61369751d232fa98246366f873f39ca0fc26
glackyy <othmanesticky@gmail.com>
2024-01-18 08:15:22 +0100

    3-glam_rock

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
 -- A Script that selects band_name, lifespan column
+
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('GLAM ROCK', IFNULL(style, "")) > 0
