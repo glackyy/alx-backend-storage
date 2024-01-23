1c89a37a755b89e623e057a48536330955a1b01d
glackyy <othmanesticky@gmail.com>
2024-01-23 07:46:58 +0100

    8-all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""Listing all documents"""
+import pymongo
+
+def list_all(mongo_collection):
+    """Function listing all doc in a collection"""
+    if not mongo_collection:
+        return []
+    docs = mongo_collection.find()
+    return [p for p in docs]
