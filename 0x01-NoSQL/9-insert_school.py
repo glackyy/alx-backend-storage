3f11d015e60765f117c5e4c253d71dc409910246
glackyy <othmanesticky@gmail.com>
2024-01-23 07:50:19 +0100

    9-insert_school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""Inserting a doc"""
+import pymongo
+
+
+def insert_scool(mongo_collection):
+    """inserting a doc into a collection"""
+    dt = mongo_collection.insert_one()
+    return dt.inserted_id
