35ea575ae919a37ff934756a0120f299d9f8f3e3
glackyy <othmanesticky@gmail.com>
2024-01-23 08:22:18 +0100

    101-students

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+"""All students sorted by avr score"""
+
+
+def top_students(mongo_collection):
+    """Returning all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
+    ])
