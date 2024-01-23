22aad8db87387f3136f3a0a278cab91a9c83f856
glackyy <othmanesticky@gmail.com>
2024-01-23 07:51:07 +0100

    9-insert_school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,5 +5,5 @@ import pymongo
 
 def insert_school(mongo_collection, **kwargs):
     """inserting a doc into a collection"""
-    dt = mongo_collection.insert_one()
+    dt = mongo_collection.insert_one(kwargs)
     return dt.inserted_id
