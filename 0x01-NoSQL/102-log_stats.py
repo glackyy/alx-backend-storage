c4d8c2dc91a703927819743c831d1f61645b3fab
glackyy <othmanesticky@gmail.com>
2024-01-23 08:26:04 +0100

    102-log_stats

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -4,7 +4,10 @@ from pymongo import MongoClient
 
 
 def nginx_stats_check():
-
+    cl = MongoClient()
+    col = cl.logs.nginx
+    num_of_documents = col.count_documents({})
+    print("{} logs".format(num_of_documents))
+    print("Methods:")
 
 if __name__ == "__main__":
-    
