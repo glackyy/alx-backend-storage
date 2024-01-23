c78e8644049bda0738f73a27b3ce3e9d507ad44e
glackyy <othmanesticky@gmail.com>
2024-01-23 08:09:21 +0100

    12-log_stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,7 +3,17 @@
 from pymongo import MongoClient
 
 
+METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+
+def log_stats(mongo_collection, option=None):
+    """Providing stats about Nginx logs"""
+    items = {}
+    if option:
+        val = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        prin(f"\tmethod {option}: {val}")
+        return
 
 
 if __name__ == "__main__":
-    
