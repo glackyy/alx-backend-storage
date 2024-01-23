2670c3d9d10ea4318333a58541438fcf639f1fc5
glackyy <othmanesticky@gmail.com>
2024-01-23 08:01:08 +0100

    11-school_by_topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Listing school topics"""
+
+
+def school_by_topic(mongo_collection, topic):
+    """Returning list of school having a specific topic"""
+    return mongo_collection.find({"topic": {"$in": [topic]}})
