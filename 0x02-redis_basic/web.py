3faeaab81158540b5a352ca0eb412284c7735345
glackyy <othmanesticky@gmail.com>
2024-01-24 16:09:03 +0100

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,1 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Web Caching and tracker"""
+import requests
+import redis
+from functools import wraps
+
+
+store = redis.Redis()
+
+
+def ct_url_access(method):
+    """Decorator counting times a url is accessed"""
+    @wraps(method)
+    def wrapper(url):
+        c_key = "cached:" + url
+        c_data = store.get(c_key)
+        if c_data:
+            return c_data.decode("utf-8")
