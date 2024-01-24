90faf6730ff4aeb01a7ed15be8a8360ad56cb109
glackyy <othmanesticky@gmail.com>
2024-01-24 10:34:45 +0100

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Function declares a redis class and methods"""
+import redis
+from uuid import uuid4
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+class Cache:
+    """Declaring a Cache class"""
+    def __init__(self):
+        """init to store an instance and flush"""
+        self.__redis = redis.Redis(host='localhost', port=6379, db=0)
+        self.__redis.flushdb()
+
+
+def count_calls(method: Callable) -> Callable:
+    """Counting how many times methods of the class Cache are called"""
+    k = method.__qualname__
